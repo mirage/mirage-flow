205304f0c4d474686e394f83260c9e2fdb718d42
Hannes Mehnert <hannes@mehnert.org>
2023-12-21 18:33:12 +0100

    adjust documentation after discussion with @reynir @dinosaure
    (1) close/shutdown is recursive
    (2) read/write errors do not lead to closed sockets

--- src/mirage_flow.mli
+++ src/mirage_flow.mli
@@ -50,49 +50,61 @@ module type S = sig
   (** [pp_write_error] is the pretty-printer for write errors. *)
 
   type flow
-  (** The type for flows. A flow represents the state of a single
-      reliable stream that is connected to an endpoint. *)
+  (** The type for flows. A flow represents the state of a single reliable
+      stream that is connected to an endpoint. *)
 
   val read: flow -> (Cstruct.t or_eof, error) result Lwt.t
-  (** [read flow] blocks until some data is available and returns a
-      fresh buffer containing it.
+  (** [read flow] blocks until some data is available and returns a fresh buffer
+      containing it.
 
       The returned buffer will be of a size convenient to the flow
       implementation, but will always have at least 1 byte.
 
-      If the remote endpoint calls [close] then calls to [read] will
-      keep returning data until all the in-flight data has been read.
-      [read flow] will return [`Eof] when the remote endpoint has
-      called [close] and when there is no more in-flight data.
-   *)
+      When [read] returns [`Eof] or an error, the [flow] should be [close]d (or
+      [shutdown]ed) by the client. Once [read] returned [`Eof] or an error, no
+      subsequent [read] call will be successful. *)
 
   val write: flow -> Cstruct.t -> (unit, write_error) result Lwt.t
-  (** [write flow buffer] writes a buffer to the flow. There is no
-      indication when the buffer has actually been read and, therefore,
-      it must not be reused.  The contents may be transmitted in
-      separate packets, depending on the underlying transport. The
-      result [Ok ()] indicates success, [Error `Closed] indicates that the
-      connection is now closed and therefore the data could not be
-      written.  Other errors are possible. *)
+  (** [write flow buffer] writes a buffer to the flow. There is no indication
+      when the buffer has actually been read and, therefore, it must not be
+      reused. The contents may be transmitted in separate packets, depending on
+      the underlying transport. The result [Ok ()] indicates success,
+      [Error `Closed] indicates that the connection is now closed and therefore
+      the data could not be written. Other errors are possible.
+
+      If [write] returns an error, [close] (or [shutdown]) should be called on
+      the [flow] by the client. Once [write] returned an error, no subsequent
+      [write] or [writev] call will be successful. *)
 
   val writev: flow -> Cstruct.t list -> (unit, write_error) result Lwt.t
-  (** [writev flow buffers] writes a sequence of buffers to the flow.
-      There is no indication when the buffers have actually been read and,
-      therefore, they must not be reused. The
-      result [Ok ()] indicates success, [Error `Closed] indicates that the
-      connection is now closed and therefore the data could not be
-      written.  Other errors are possible. *)
+  (** [writev flow buffers] writes a sequence of buffers to the flow. There is
+      no indication when the buffers have actually been read and, therefore,
+      they must not be reused. The result [Ok ()] indicates success,
+      [Error `Closed] indicates that the connection is now closed and therefore
+      the data could not be written. Other errors are possible.
+
+      If [writev] returns an error, [close] (or [shutdown]) should be called on
+      the [flow] by the client. Once [writev] returned an error, no subsequent
+      [writev] or [write] call will be successful. *)
 
   val shutdown : flow -> [ `read | `write | `read_write ] -> unit Lwt.t
   (** [shutdown flow mode] shuts down the [flow] for the specific [mode]:
-      A flow which is [shutdown `read] (or [`read_write] will never be [read]
-      again (future calls will return [`Eof]); a flow which is [shutdown `write]
-      (or [`read_write]) flushes all pending writes and signals the remote
-      endpoint there won't be any future [write]. In TCP, a FIN is sent. *)
+      A flow which is [shutdown `read] (or [`read_write]) will never be [read]
+      again (subsequent calls will return [`Eof]); a flow which is
+      [shutdown `write] (or [`read_write]) flushes all pending writes and
+      signals the remote endpoint there won't be any future [write] or [writev]
+      calls (subsequent calls will return [`Closed]). E.g. in TCP, the
+      signalling is done by sending a segment with the FIN flag.
+
+      If this [flow] is layered upon another [flow'] (e.g. TLS over TCP),
+      [shutdown] on the underlying [flow'] is executed. *)
 
   val close: flow -> unit Lwt.t
   (** [close flow] terminates the [flow] and frees all associated data. Any
       subsequent [read] or [write] will return an error. A subsequent [close]
       will not do anything (esp. not raising an exception), but it may log an
-      error. *)
+      error.
+
+      If this [flow] is layered upon another [flow'] (e.g. TLS over TCP),
+      [close] on the underlying [flow'] is executed. *)
 end
